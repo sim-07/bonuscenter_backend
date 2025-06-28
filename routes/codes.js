a912e19057cd56dd42a3533180c2d17257327677
sim-07 <simone07andreotti@gmail.com>
2025-06-28 17:19:35 +0200

    modified:   routes/codes.js

--- routes/codes.js
+++ routes/codes.js
@@ -7,7 +7,7 @@ require('dotenv').config();
 const jwt = require('jsonwebtoken');
 
 router.post('/post_code', async (req, res) => {
-    const { title, code, description, brand, bonus_value } = req.body;
+    const { title, name, code, description, brand, bonus_value } = req.body;
 
     if (!title || !code || !description || !bonus_value) {
         return res.status(400).json({ error: 'Missing required fields' });
@@ -31,6 +31,7 @@ router.post('/post_code', async (req, res) => {
             .insert([
                 {
                     user_id: user_id,
+                    name,
                     title,
                     brand,
                     code,
@@ -75,13 +76,41 @@ router.post('/get_user_codes', async (req, res) => {
 
         if (!data || data.length === 0) {
             return res.status(200).json({ message: 'No referral codes yet', data: [] });
-          }
+        }
 
         res.status(200).json({ message: "User codes recovered successfully", data });
     } catch (err) {
         console.error(err);
         res.status(500).json({ error: 'Something went wrong', details: err.message });
     }
+});
+
+
+router.post('/get_referral_codes', async (req, res) => {
+    const { name } = req.body;
+
+    try {
+        const { data, error } = await supabase
+            .from('referral_codes')
+            .select('*')
+            .eq('name', name);
+
+        if (error) {
+            return res.status(500).json({ error: error.message });
+        }
+
+        if (!data || data.length === 0) {
+            return res.status(200).json({ message: 'No referral codes yet', data: [] });
+        }
+
+        console.log("DATA get_referral_codes: ", data)
+
+        res.status(200).json({ message: "User codes recovered successfully", data });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Something went wrong', details: err.message });
+    }
+
 });
 
 module.exports = router;
